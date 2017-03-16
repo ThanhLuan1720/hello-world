9dbd1fa933b763378fcd2b653fd90b0d523c3d16
Thanh Luan <dothanhluan1720@gmail.com>
2017-03-16 10:58:42 +0700

    add folder

---
+++ app/app.js
@@ -0,0 +1,23 @@
+// Define the `phonecatApp` module
+var phonecatApp = angular.module('phonecatApp', []);
+
+// Define the `PhoneListController` controller on the `phonecatApp` module
+// phonecatApp.controller('PhoneListController', function PhoneListController($scope) {
+//   $scope.phones = [
+//     {
+//       name: 'Nexus S',
+//       snippet: 'Fabcx.',
+//       age: 1
+//     }, {
+//       name: 'Fabcx',
+//       snippet: 'The Next, Next Generation tablet.',
+//       age:2
+//     }, {
+//       name: 'MOTOROLA XOOM™',
+//       snippet: 'The Next, Next Generation tablet.',
+//       age:3
+//     }
+//   ];
+//   this.orderProp = 'age';
+// });
+
